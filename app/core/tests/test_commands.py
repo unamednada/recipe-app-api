84adb927aa2cd30e2925a1e5f754c586f7adfd76
unamednada <dias.gustavo.dias@gmail.com>
2022-12-30 17:01:29 -0300

    fix: import patch

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -2,7 +2,7 @@
 Test custom Django management commands
 """
 
-from unittest import patch
+from unittest.mock import patch
 from psycopg2 import OperationalError as Psycopg2Error
 from django.core.management import call_command
 from django.db.utils import OperationalError
