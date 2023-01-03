aa881ca943c6aefe5589456271fca7e57482dc21
unamednada <dias.gustavo.dias@gmail.com>
2023-01-03 11:58:58 -0300

    feat: recipe detail view

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -14,3 +14,11 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
+        read_only_fields = ['id']
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -13,11 +13,20 @@ from recipe import serializers
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe API"""
 
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         """Retrieve the recipes for the authenticated user"""
+
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Return appropriate serializer class"""
+
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
