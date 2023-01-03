95637da73814cee4990a57290ac1d9056c2dd15b
unamednada <dias.gustavo.dias@gmail.com>
2023-01-03 18:56:53 -0300

    feat: delete tag

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -38,6 +38,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
 
 class TagViewSet(
+    mixins.DestroyModelMixin,
     mixins.UpdateModelMixin,
     mixins.ListModelMixin,
     viewsets.GenericViewSet
