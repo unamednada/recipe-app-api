7a2f8bff2591f9351ccc5ba94d24ad823273759f
unamednada <dias.gustavo.dias@gmail.com>
2023-01-03 18:56:01 -0300

    feat: test delete tag

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -92,3 +92,14 @@ class PrivateTagsApiTests(TestCase):
         tag.refresh_from_db()
 
         self.assertEqual(tag.name, payload['name'])
+
+    def test_delete_tag(self):
+        """Test deleting a tag"""
+
+        tag = Tag.objects.create(user=self.user, name='tag1')
+
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Tag.objects.filter(id=tag.id).exists())
