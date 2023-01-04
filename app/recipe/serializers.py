222623a8c71c9ff6a65db5f087c993cc3423ef6c
unamednada <dias.gustavo.dias@gmail.com>
2023-01-04 12:27:40 -0300

    feat: update recipe tags

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -26,11 +26,9 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
 
-    def create(self, validated_data):
-        """Create a new recipe"""
+    def _get_or_create_tags(self, tags, recipe):
+        """Get or create tags"""
 
-        tags = validated_data.pop('tags', [])
-        recipe = Recipe.objects.create(**validated_data)
         auth_user = self.context['request'].user
 
         for tag in tags:
@@ -40,7 +38,31 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tags.add(tag_obj)
 
+    def create(self, validated_data):
+        """Create a new recipe"""
+
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
+
         return recipe
+
+    def update(self, instance, validated_data):
+        """Update a recipe"""
+
+        tags = validated_data.pop('tags', None)
+
+        if tags is not None:
+            instance.tags.clear()
+
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
