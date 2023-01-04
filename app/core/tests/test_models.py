7da5c9491c0c864115b3323caba17a4afbbf2a42
unamednada <dias.gustavo.dias@gmail.com>
2023-01-04 15:30:18 -0300

    feat: test add ingredient

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -94,3 +94,15 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_create_ingredient(self):
+        """Test creating an ingredient"""
+
+        user = create_user()
+
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='ingredient'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
