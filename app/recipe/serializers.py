375019e62e02092b4afcae6f34d63c5954b3a2f0
unamednada <dias.gustavo.dias@gmail.com>
2023-01-04 18:24:48 -0300

    feat: create ingredient

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -33,10 +33,15 @@ class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipe objects"""
 
     tags = TagSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
+        fields = [
+            'id', 'title', 'time_minutes', 'price', 'link',
+            'tags',
+            'ingredients',
+        ]
         read_only_fields = ['id']
 
     def _get_or_create_tags(self, tags, recipe):
@@ -50,13 +55,27 @@ class RecipeSerializer(serializers.ModelSerializer):
                 **tag
             )
             recipe.tags.add(tag_obj)
+    
+    def _get_or_create_ingredients(self, ingredients, recipe):
+        """Get or create ingredients"""
+
+        auth_user = self.context['request'].user
+
+        for ingredient in ingredients:
+            ingredient_obj, created = Ingredient.objects.get_or_create(
+                user=auth_user,
+                **ingredient
+            )
+            recipe.ingredients.add(ingredient_obj)
 
     def create(self, validated_data):
         """Create a new recipe"""
 
         tags = validated_data.pop('tags', [])
+        ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data)
         self._get_or_create_tags(tags, recipe)
+        self._get_or_create_ingredients(ingredients, recipe)
 
         return recipe
 
